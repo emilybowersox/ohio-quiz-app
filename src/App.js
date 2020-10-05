5b4a72c1d9413759672371a22ccd9d506ae0a44a
Emily Bowersox <emily.bowersox100@gmail.com>
2020-10-05 17:05:54 -0500

    created questions array for quiz

--- src/App.js
+++ src/App.js
@@ -3,24 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //an array of questions
+  const questions = [
+      //each question is an object with the properties of question and answer options
+    { questionText: 'What is the capital of Ohio?',
+      //answerOptions is an array of answerOption objects with text and isCorrect properties
+      answerOptions: [
+        { answerText: 'Dublin', isCorrect: false },
+        { answerText: 'Columbus', isCorrect: true },
+        { answerText: 'Cleveland', isCorrect: false },
+        { answerText: 'Cincinnati', isCorrect: false },
+      ], },
+
+    { questionText: 'What is the birthplace of twenty one pilots?',
+      answerOptions: [
+        { answerText: 'Sandusky', isCorrect: false },
+        { answerText: 'Cincinnati', isCorrect: false },
+        { answerText: 'Columbus', isCorrect: true },
+        { answerText: 'Troy', isCorrect: false },
+      ], },
+
+    { questionText: 'In what Ohio city did Jeni\'s founder, Jeni Britton Bauer, begin her ice cream empire?',
+      answerOptions: [
+        { answerText: 'Columbus', isCorrect: true },
+        { answerText: 'Xenia', isCorrect: false },
+        { answerText: 'Cleveland', isCorrect: false },
+        { answerText: 'Hilliard', isCorrect: false },
+      ], },
+
+    { questionText: 'The nationally award winning Columbus zoo resides in which city?',
+      answerOptions: [
+        { answerText: 'New Albany', isCorrect: false },
+        { answerText: 'Worthington', isCorrect: false },
+        { answerText: 'Cincinnati', isCorrect: false },
+        { answerText: 'Columbus', isCorrect: true },
+      ], },
+  ]
+
+
+
+
+
+
+
 }
 
 export default App;
